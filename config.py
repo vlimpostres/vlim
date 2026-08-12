3ab8b6dd8f040b6d4125687327108208b1935ecb
Dario Rodriguez <dariorodq30@gmail.com>
2026-08-11 22:02:36 -0500

    config.py usa variables de entorno, ya no se excluye del repo

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'connect_args': {
+            'ssl': {}
+        }
+    }
+    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
